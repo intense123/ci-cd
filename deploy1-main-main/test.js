cf1d4d9d622eb25ad6b7d7f05ce3cfa69dd4bcdb
Reza Abdullah <bsse1335@iit.du.ac.bd>
2025-01-07 14:40:32 +0600

    Update test.js

--- deploy1-main-main/test.js
+++ deploy1-main-main/test.js
@@ -7,7 +7,7 @@ describe('GET /', () => {
   it('should return Hello World', async () => {
     const response = await request(server).get('/');
     console.log(response.text);  
-    assert.strictEqual(response.text, 'Hello World Pass');
+    assert.strictEqual(response.text, 'Hello World Pass14');
   });
 });
 
